2eb8ebd32487851a0b0070c7bc3550b817260acd
BianLong0618 <bianlong0618@qq.com>
2022-10-13 11:21:47 -0400

    Finished the code in 44 from the Section 6.

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,10 +6,29 @@ class UserModel extends \CodeIgniter\Model
 {
     protected $table = 'user';
 
-    protected $allowedFields = ['name', 'email'];
+    protected $allowedFields = ['name', 'email', 'password'];
 
-    // Link this model to the Entity class we made.
     protected $returnType = 'App\Entities\User';
 
     protected $useTimestamps = true;
+
+    //  表示在field password插入前执行hashPassword callback函数。
+    protected $beforeInsert = ['hashPassword'];
+
+    //  hashPassword callback函数
+    //  $data contains the value from the form.
+    protected function hashPassword(array $data)
+    {
+        //  如果form的传入数据($data)中有password，则对password进行hash；
+        //  用default算法进行hash。
+        if (isset($data['data']['password'])) {
+
+            $data['data']['password_hash'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+
+            //  删掉password？
+            unset($data['data']['password']);
+        }
+
+        return $data;
+    }
 }
