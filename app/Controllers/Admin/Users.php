83c99c02903f9068134f4a08a4c0e94f71b37bba
BianLong0618 <bianlong0618@qq.com>
2022-11-01 19:58:13 -0400

    Update the codebase from video 71 of Section 10.

--- app/Controllers/Admin/Users.php
+++ app/Controllers/Admin/Users.php
@@ -6,8 +6,22 @@ namespace App\Controllers\Admin;
 //  即导致not found
 class Users extends \App\Controllers\BaseController
 {
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new \App\Models\UserModel;
+    }
+
     public function getIndex()
     {
-        return view('Admin/Users/index');
+        //  得到以id排序的user列表，且每页显示5条。
+        $users = $this->model->orderBy('id')
+            ->paginate(5);
+        //  将user数据和分页信息传到前端。
+        return view('Admin/Users/index', [
+            'users' => $users,
+            'pager' => $this->model->pager
+        ]);
     }
 }
