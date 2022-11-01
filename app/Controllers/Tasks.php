2289d6385b58a439671757093b80bae97a26f4cd
BianLong0618 <bianlong0618@qq.com>
2022-11-01 10:26:15 -0400

    Update the codebase from video 67 of Section 9.

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -9,31 +9,33 @@ class Tasks extends BaseController
 {
     private $model;
 
+    private $current_user;
+
     public function __construct()
     {
-        $this -> model = new \App\Models\TaskModel;
+        $this->model = new \App\Models\TaskModel;
+        $this->current_user = service("auth")->getCurrentUser();
     }
 
 
     public function getIndex()
     {
-        $auth = service('auth');
-        $user = $auth->getCurrentUser();
-
-        $data = $this->model->getTasksByUserId($user->id);
+        $data = $this->model->getTasksByUserId($this->current_user->id);
 
         return view("Tasks/index", ['tasks' => $data]);
     }
 
 
-    public function getShow($id){
+    public function getShow($id)
+    {
 
-        $task = $this -> getTaskOr404($id);
+        $task = $this->getTaskOr404($id);
 
-        return view("Tasks/show", ['task' => $task ]);
+        return view("Tasks/show", ['task' => $task]);
     }
 
-    public function getNew(){
+    public function getNew()
+    {
         $task = new Task;
 
         return view("Tasks/new", [
@@ -41,71 +43,75 @@ class Tasks extends BaseController
         ]);
     }
 
-    public function postCreate(){
-        $task = new Task($this -> request -> getPost() );
+    public function postCreate()
+    {
+        $task = new Task($this->request->getPost());
 
-        $user = service("auth") -> getCurrentUser();
 
-        $task -> user_id = $user -> id;
+        $task->user_id = $this->current_user->id;
 
-        if ($this -> model -> insert($task)){
-            return redirect() -> to("tasks/show/{$this -> model -> insertID}")
-                -> with("info", "Task created successfully");
-        }else{
-            return redirect() -> back()
-                -> with("errors", $model -> errors())
-                -> with("warning", "Invalid data")
-                -> withInput();
+        if ($this->model->insert($task)) {
+            return redirect()->to("tasks/show/{$this -> model -> insertID}")
+                ->with("info", "Task created successfully");
+        } else {
+            return redirect()->back()
+                ->with("errors", $model->errors())
+                ->with("warning", "Invalid data")
+                ->withInput();
         }
     }
 
-    public function getEdit($id){
+    public function getEdit($id)
+    {
 
-        $task = $this -> getTaskOr404($id);
+        $task = $this->getTaskOr404($id);
 
         return view("tasks/edit", ["task" => $task]);
     }
 
-    public function postUpdate($id){
+    public function postUpdate($id)
+    {
 //     不用Entity， 直接对model中的row进行操作的方法。
 //        $result = $model -> update($id, [
 //           "description" => $this -> request -> getPost("description")
 //        ]);
 
-        $task = $this -> getTaskOr404($id);
+        $task = $this->getTaskOr404($id);
 
 //      安全性Sentinel，如果post进来的数据有user_id就直接删除。
-        $post = $this -> request -> getPost();
+        $post = $this->request->getPost();
         unset($post["user_id"]);
 
-        $task -> fill($post);
+        $task->fill($post);
 
-        if( !$task -> hasChanged()){
-            return redirect() -> back()
-                -> with("error", $this -> model -> errors())
-                -> with("warning", "Nothing to update")
-                -> withInput();
+        if (!$task->hasChanged()) {
+            return redirect()->back()
+                ->with("error", $this->model->errors())
+                ->with("warning", "Nothing to update")
+                ->withInput();
         }
 
-        if ( $this -> model -> save($task)){
-            return redirect() -> to("/tasks/show/$id")
-                -> with("info", "Task updated successfully!");
-        }else{
-            return redirect() -> back()
-                              -> with("warning", "Invalid data")
-                              -> withInput();
+        if ($this->model->save($task)) {
+            return redirect()->to("/tasks/show/$id")
+                ->with("info", "Task updated successfully!");
+        } else {
+            return redirect()->back()
+                ->with("warning", "Invalid data")
+                ->withInput();
         }
     }
 
 
-    public function getDelete($id){
-        $task = $this ->getTaskOr404($id);
+    public function getDelete($id)
+    {
+        $task = $this->getTaskOr404($id);
 
         return view("Tasks/delete", ["task" => $task]);
 
     }
 
-    public function postDelete($id){
+    public function postDelete($id)
+    {
         $task = $this->getTaskOr404($id);
 
         $this->model->delete($id);
@@ -115,9 +121,8 @@ class Tasks extends BaseController
     }
 
 
-    private function getTaskOr404($id){
-        $user = service('auth')->getCurrentUser();
-
+    private function getTaskOr404($id)
+    {
         /*
         $task = $this->model->find($id);
 
@@ -128,7 +133,7 @@ class Tasks extends BaseController
         }
         */
 
-        $task = $this->model->getTaskByUserId($id, $user->id);
+        $task = $this->model->getTaskByUserId($id, $this->current_user->id);
 
         if ($task === null) {
 
