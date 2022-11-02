f0babba8c2deca374a6554f36bec2c43733706a6
BianLong0618 <bianlong0618@qq.com>
2022-11-02 11:01:05 -0400

    Update the codebase from video 80 of Section 11.

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $model = new \App\Models\UserModel;
+
+        $data = [
+            'name'     => 'Admin',
+            'email'    => 'admin@example.com',
+            'password' => 'secret',
+            'is_admin' => true
+        ];
+        //  表示在插入前略过validation
+        $model->skipValidation(true)
+            //  无视model中的allowedfields protected。
+            ->protect(false)
+            ->insert($data);
+        //  如果成功则显示errors为空数组。
+        dd($model->errors());
+    }
+}
