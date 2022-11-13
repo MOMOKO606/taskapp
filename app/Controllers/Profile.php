17ea9197e502e384d2a24deb68f3d2dd3fcc067a
BianLong0618 <bianlong0618@qq.com>
2022-11-13 16:13:14 -0500

    Update the codebase from video 106 in Section 14.

--- app/Controllers/Profile.php
+++ app/Controllers/Profile.php
@@ -24,4 +24,30 @@ class Profile extends BaseController
             'user' => $this->user
         ]);
     }
+
+    public function postUpdate()
+    {
+        $this->user->fill($this->request->getPost());
+
+        if ( ! $this->user->hasChanged()) {
+
+            return redirect()->back()
+                ->with('warning', 'Nothing to update')
+                ->withInput();
+        }
+
+        $model = new \App\Models\UserModel;
+
+        if ($model->save($this->user)) {
+
+            return redirect()->to("/profile/show")
+                ->with('info', 'Details updated successfully');
+        } else {
+
+            return redirect()->back()
+                ->with('errors', $model->errors())
+                ->with('warning', 'Invalid data')
+                ->withInput();
+        }
+    }
 }
